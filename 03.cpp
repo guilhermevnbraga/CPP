79755f3fd4b1abb66e39c8aed181d6c66cbc667e
Guilherme Braga <crazyboysfh3@gmail.com>
2023-07-07 23:55:20 -0300

    update

---
+++ 03.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main() {
+    int number1;
+    int number2;
+
+    cin >> number1 >> number2;
+
+    if (number1 == number2)
+        cout << number1 << " == " << number2 << endl;
+    if (number1 != number2)
+        cout << number1 << " != " << number2 << endl;
+    if (number1 < number2)
+        cout << number1 << " < " << number2 << endl;
+    if (number1 > number2)
+        cout << number1 << " > " << number2 << endl;
+    if (number1 <= number2)
+     cout << number1 << " <= " << number2 << endl;
+    if (number1 >= number2)
+        cout << number1 << " >= " << number2 << endl;
+
+    return 0;
+}
