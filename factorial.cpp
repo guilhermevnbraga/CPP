305aa8838aa0625d82f36995aa51e5894070f7ec
Guilherme Braga <crazyboysfh3@gmail.com>
2023-07-11 23:35:52 -0300

    update

--- factorial.cpp
+++ factorial.cpp
@@ -23,5 +23,5 @@ unsigned long factorial(unsigned long number)
     if (number <= 1)
         return 1;
     else
-        return number * factorial(number - 1);
+        return number * factorial(number- 1);
 }
