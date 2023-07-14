b0ce817cc108db92a4d6dfc741354b868d865c1f
Guilherme Braga <crazyboysfh3@gmail.com>
2023-07-13 21:35:40 -0300

    update

---
+++ hanoi.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+void hanoi(int, int, int, int);
+
+int main(){
+    return 0;
+}
+
+void hanoi(int discNumber, int initialPin, int wishedPin, int storagePin){
+    
+}
