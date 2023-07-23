59718ba379858dbf2115d8b242e296cadca6dae1
Guilherme Braga <crazyboysfh3@gmail.com>
2023-07-22 23:58:40 -0300

    g

---
+++ test2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+void print(const char);
+
+int main()
+{
+    print("amogus");
+}
+
+void print(const char* string[0])
+{
+    cout << string;
+}
